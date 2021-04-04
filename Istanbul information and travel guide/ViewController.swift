9a53df6afcc10548b70c7d18e4fc2f841512c5f9
Burak Arslan <43721185+Burak-Arslan@users.noreply.github.com>
2021-04-05 01:09:52 +0300

    - Lottie file kütüphanesi entegre edildi
    - Lottie file kütüphanesi entegre edildi

--- Istanbul information and travel guide/ViewController.swift
+++ Istanbul information and travel guide/ViewController.swift
@@ -1,9 +1,3 @@
-//
-//  ViewController.swift
-//  Istanbul information and travel guide
-//
-//  Created by Burak Arslan on 5.04.2021.
-//
 
 import UIKit
 
@@ -11,9 +5,6 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
