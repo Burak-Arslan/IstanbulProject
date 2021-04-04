765fc41ee28b82b74f3dab471d7006a16506f456
Burak Arslan <43721185+Burak-Arslan@users.noreply.github.com>
2021-04-05 00:53:52 +0300

    - first
    - first

---
+++ Istanbul information and travel guide/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Istanbul information and travel guide
+//
+//  Created by Burak Arslan on 5.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
